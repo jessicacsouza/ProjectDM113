93efe106d06fa637865e0d7a42c05b10bb427022
jessicacsouza <jessicasouza@gec.inatel.br>
2021-11-13 18:23:55 -0200

    Criando módulo Provedor de Serviços de Estque

---
+++ EstoqueServiceHost/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using EstoqueProduto;
+
+namespace EstoqueServiceHost
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost estoqueServiceHost = new ServiceHost(typeof(ServicoEstoque));
+            estoqueServiceHost.Open();
+            Console.WriteLine("Service Running");
+
+            Console.ReadLine();
+            Console.WriteLine("Service Stopping");
+            estoqueServiceHost.Close();
+        }
+    }
+}
